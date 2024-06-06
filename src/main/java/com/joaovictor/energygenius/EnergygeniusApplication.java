a0b6e37bfaa8abc80d9851976e973eb401e2a5bb
J7ohn <jvfonseca1349@gmail.com>
2024-06-06 01:51:45 -0300

    project created

---
+++ src/main/java/com/joaovictor/energygenius/EnergygeniusApplication.java
@@ -0,0 +1,13 @@
+package com.joaovictor.energygenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergygeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergygeniusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaovictor/energygenius/EnergygeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovictor.energygenius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergygeniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
