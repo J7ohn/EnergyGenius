9b74f58f94f2cd3e241fcdb9e42ebc3965ffb31d
J7ohn <jvfonseca1349@gmail.com>
2024-06-06 02:18:00 -0300

    service layer

---
+++ src/main/java/com/joaovictor/energygenius/services/UserService.java
@@ -0,0 +1,26 @@
+package com.joaovictor.energygenius.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaovictor.energygenius.entities.User;
+import com.joaovictor.energygenius.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
